1567b1953cebbabe6f5766abd60625f0b515f70e
aneumill <71047690+aneumill@users.noreply.github.com>
2021-02-03 20:20:38 -0700

    Some changes.

--- JoelHiltonMovieApp_AdamNeumiller/Controllers/HomeController.cs
+++ JoelHiltonMovieApp_AdamNeumiller/Controllers/HomeController.cs
@@ -7,6 +7,12 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+/* I DOUBLE FILTERED IN MY CODE. I DON'T ALLOW INDEPENEDNCE DAY TO BE STORED AT ALL WITH THE ACTION ADDMOVIE(). I ALSO DO NOT ALLOW IT TO BE DISPLAYED
+ 
+ AS SHOWN IN THE MYMOVIES() ACTION. */
+
+
+
 namespace JoelHiltonMovieApp_AdamNeumiller.Controllers
 {
     public class HomeController : Controller
@@ -34,11 +40,14 @@ namespace JoelHiltonMovieApp_AdamNeumiller.Controllers
         {
             if (ModelState.IsValid)
             {
-
+                //THIS IS THE FIRST FILTER. IF THE MODEL IS VALID THEN IT DROPS IN TO THE IF STATEMENT. IF THE VALUE IS INDPENDENCE DAY THEN IT 
+                // RETURNS A VIEW SAYING THAT IT COULD NOT BE ADDED. IF IT IS NOT INDPENDENCE DAY THEN THEN A VIEW IS RETURNED SAYING THAT THE 
+                // FORM WAS SUCESSFULLY SUBMITED. THE OTHER FILTER USING LINQ IS SHOWN BELOW. 
                 if (appResponse.Title.ToUpper() != "INDEPENDENCE DAY")
                 {
                     JoelsCollection.AddMovie(appResponse);
                     return View("Confirmation", appResponse);
+    
                 }
                 else
                 {
@@ -51,11 +60,18 @@ namespace JoelHiltonMovieApp_AdamNeumiller.Controllers
                 return View();
             }
         }
-        
-        
+
+       
+
+
+        //THIS IS ME FUFILLLING THE FILTERING PART WITH INDEPENDENCE DAY USING LINQ, PER WHAT I WAS ABLE TO FIGURE OUT IN THE TEXTBOOK.
         public IActionResult MyMovies()
         {
-            return View(JoelsCollection.MovieCollection);
+            //linq filtering
+             return View(JoelsCollection.MovieCollection.Where(r => r.Title.ToUpper() != "INDEPENDENCE DAY"));
+             
+
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
